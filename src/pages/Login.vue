073d1935a1efd35a4d0044f72bcba6a0cec0b2b7
cflv5 <bedirhanakcay@icloud.com>
2022-06-18 11:10:42 +0300

    feat: create login and logout page and its logic

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,32 +1,26 @@
 <template>
     <div class="surface-0 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+        <Toast />
         <div class="grid justify-content-center p-2 lg:p-0" style="min-width:80%">
             <div class="col-12 mt-5 xl:mt-0 text-center">
-                <img :src="'layout/images/logo-' + logoColor + '.svg'" alt="Sakai logo" class="mb-5" style="width:81px; height:60px;">
+                <img :src="'images/logo.png'" alt="Sakai logo" class="mb-5">
             </div>
-            <div class="col-12 xl:col-6" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
-                <div class="h-full w-full m-0 py-7 px-4" style="border-radius:53px; background: linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0));">
-                    <div class="text-center mb-5">
-                        <img src="layout/images/avatar.png" alt="Image" height="50" class="mb-3">
-                        <div class="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
-                        <span class="text-600 font-medium">Sign in to continue</span>
-                    </div>
-                
+            <div class="col-12 xl:col-6"
+                style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
+                <div class="h-full w-full m-0 py-7 px-4"
+                    style="border-radius:53px; background: linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0));">
                     <div class="w-full md:w-10 mx-auto">
                         <label for="email1" class="block text-900 text-xl font-medium mb-2">Email</label>
-                        <InputText id="email1" v-model="email" type="text" class="w-full mb-3" placeholder="Email" style="padding:1rem;" />
-                
+                        <InputText id="email1" required="true" v-model="email" type="text" class="w-full mb-3"
+                            placeholder="Email" style="padding:1rem;" />
+
                         <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" inputStyle="padding:1rem"></Password>
-                
-                        <div class="flex align-items-center justify-content-between mb-5">
-                            <div class="flex align-items-center">
-                                <Checkbox id="rememberme1" v-model="checked" :binary="true" class="mr-2"></Checkbox>
-                                <label for="rememberme1">Remember me</label>
-                            </div>
-                            <a class="font-medium no-underline ml-2 text-right cursor-pointer" style="color: var(--primary-color)">Forgot password?</a>
-                        </div>
-                        <Button label="Sign In" class="w-full p-3 text-xl"></button>
+                        <Password id="password1" :feedback="false" v-model="password" placeholder="Password"
+                            :toggleMask="true" class="w-full mb-3" inputClass="w-full" inputStyle="padding:1rem"
+                            @keyup.enter="login">
+                        </Password>
+
+                        <Button label="Sign In" class="w-full p-3 text-xl" @click="login"></button>
                     </div>
                 </div>
             </div>
@@ -35,18 +29,35 @@
 </template>
 
 <script>
+import UserService from '../service/UserService';
+import util from '../util/ServiceUtil'
+
 export default {
     data() {
         return {
             email: '',
             password: '',
-            checked: false
+            submitted: false
         }
     },
-    computed: {
-        logoColor() {
-            if (this.$appState.darkTheme) return 'white';
-            return 'dark';
+
+    userService: null,
+
+    created() {
+        this.userService = new UserService(this);
+    },
+
+    methods: {
+        login() {
+            const that = this;
+            if (!this.submitted) {
+                this.submitted = true;
+                this.userService.login(this.email, this.password)
+                    .catch(e => {
+                        that.submitted = false;
+                        this.$toast.add(util.handleAxiosError(e));
+                    });
+            }
         }
     }
 }
@@ -54,12 +65,12 @@ export default {
 
 <style scoped>
 .pi-eye {
-    transform:scale(1.6);
+    transform: scale(1.6);
     margin-right: 1rem;
 }
 
 .pi-eye-slash {
-    transform:scale(1.6);
+    transform: scale(1.6);
     margin-right: 1rem;
 }
 </style>
---
+++ src/pages/Logout.vue
@@ -0,0 +1,13 @@
+<template>
+Logging out...
+</template>
+
+<script>
+    
+export default {
+    created() {
+        this.$store.commit("setTenantId", "");
+        this.$router.push("/login");
+    }
+}
+</script>
